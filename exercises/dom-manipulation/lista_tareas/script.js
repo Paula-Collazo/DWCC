7549133640cd93f305a2db0023186e660f759ac5
Paula Collazo Figueroa <pacofi2005@gmail.com>
2025-10-16 14:10:34 +0200

    commit 4

--- exercises/dom-manipulation/lista_tareas/script.js
+++ exercises/dom-manipulation/lista_tareas/script.js
@@ -4,62 +4,123 @@
 const tituloPrincipal = document.getElementById("titulo-principal");
 const formulario = document.getElementById("formulario-tarea");
 const listaTareas = document.getElementById("lista-tareas");
-const estadísticas = document.getElementById("estadisticas");
+const estadisticas = document.getElementById("estadisticas");
+
+const bntMostrarTodas = document.getElementById("btn-mostrar-todas");
+const bntMostrarCompletadas = document.getElementById("btn-mostrar-completadas");
+const bntMostrarPendientes = document.getElementById("btn-mostrar-pendientes");
 
 const inputTarea = document.querySelector("#input-tarea");
 const select = document.querySelector("#prioridad-tarea");
 const botonAgregar = document.querySelector(".btn-agregar");
 
 const botones = document.querySelectorAll(".boton");
-const itemsTareas = document.getElementsByClassName("tarea-item");
+const tarea = document.getElementsByClassName("tarea-item");
 
 // 2. FUNCIONES AUXILIARES
 // Función para crear una nueva tarea (usa createElement, appendChild, setAttribute)
 
 function crearNuevaTarea(textoTarea, prioridad) {
-    const liTarea = document.createElement("li");
-    const checkbox = document.createElement("input");
-    const spanTexto = document.createElement("span");
-    const spanPrioridad = document.createElement("span");
-    const btnEliminar = document.createElement("button");
-  
-   
-    checkbox.setAttribute("type", "checkbox");
-    
-    liTarea.classList.add("tarea-item");
-    checkbox.classList.add("tarea-checkbox");
-    spanTexto.classList.add("tarea-texto");
-    spanPrioridad.classList.add("tarea-prioridad");
-    btnEliminar.classList.add("btn-eliminar");
-    
+  const liTarea = document.createElement("li");
+  const checkbox = document.createElement("input");
+  const spanTexto = document.createElement("span");
+  const spanPrioridad = document.createElement("span");
+  const btnEliminar = document.createElement("button");
 
+  checkbox.setAttribute("type", "checkbox");
 
-    spanTexto.textContent = textoTarea;
-    spanPrioridad.textContent = prioridad;
-    btnEliminar.textContent = "eliminar";
+  liTarea.classList.add("tarea-item");
+  checkbox.classList.add("tarea-checkbox");
+  spanTexto.classList.add("tarea-texto");
+  spanPrioridad.classList.add("tarea-prioridad");
+  btnEliminar.classList.add("btn-eliminar");
 
-    liTarea.appendChild(checkbox);
-    liTarea.appendChild(spanTexto);
-    liTarea.appendChild(spanPrioridad);
-    liTarea.appendChild(btnEliminar);
-    
+  spanTexto.textContent = textoTarea;
+  spanPrioridad.textContent = prioridad;
+  btnEliminar.textContent = "eliminar";
 
+  liTarea.appendChild(checkbox);
+  liTarea.appendChild(spanTexto);
+  liTarea.appendChild(spanPrioridad);
+  liTarea.appendChild(btnEliminar);
 
-    listaTareas.appendChild(liTarea);
-  }
+  listaTareas.appendChild(liTarea);
 
+  checkbox.addEventListener("change", (e) => {
+    e.stopPropagation();
+    liTarea.classList.toggle("completada");
+    actualizarEstadisticas()
+  });
 
-  formulario.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const textoTarea = inputTarea.value.trim();
-    const prioridad = select.value;
-    crearNuevaTarea(textoTarea, prioridad);
-  })
+  btnEliminar.addEventListener("click", (e) => {
+    e.stopPropagation();
+    liTarea.remove();
+    actualizarEstadisticas()
+  });
+}
 
+function filtrarTareas(filtro) {
+  estadisticas.classList.remove("oculto");
+
+  const todasLasTareas = document.getElementsByClassName("tarea-item");
+
+  for (let tarea of todasLasTareas) {
+    const estaCompletada = tarea.classList.contains("completada");
+    switch (filtro) {
+      case "todas":
+        tarea.classList.remove("oculto");
+        break;
+      case "completadas":
+        if (estaCompletada) {
+          tarea.classList.remove("oculto");
+        } else {
+          tarea.classList.add("oculto");
+        }
+        break;
+      case "pendientes":
+        if (!estaCompletada) {
+          tarea.classList.remove("oculto");
+        } else {
+          tarea.classList.add("oculto");
+        }
+        break;
+    }
+  };
+}
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const textoTarea = inputTarea.value.trim();
+  const prioridad = select.value;
+  crearNuevaTarea(textoTarea, prioridad);
+  actualizarEstadisticas();
+
+});
+
+bntMostrarTodas.addEventListener("click", (e) => {
+  e.preventDefault();
+  filtrarTareas("todas");
+});
+
+bntMostrarCompletadas.addEventListener("click", (e) => {
+  e.preventDefault();
+  filtrarTareas("completadas");
+});
+
+bntMostrarPendientes.addEventListener("click", (e) => {
+  e.preventDefault();
+  filtrarTareas("pendientes");
+});
 // Función para actualizar estadísticas (usa textContent)
 
 function actualizarEstadisticas() {
-       
+  const containerTotal = document.getElementById("total-tareas");
+  const containerCompletadas = document.getElementById("tareas-completadas");
+  const containerPendientes = document.getElementById("tareas-pendientes");
+
+  containerTotal.textContent = document.getElementsByClassName("tarea-item").length;
+  containerCompletadas.textContent = document.getElementsByClassName("completada").length
+  containerPendientes.textContent = containerTotal.textContent - containerCompletadas.textContent
 }
 
 // Función para eliminar tarea (usa removeChild o remove, parentElement)
